d5f51e87c65217a7a83b6df9b8e5ad7a5de93a86
Alfiya Heba <alfiyaheba10@gmail.com>
2025-07-30 16:07:35 +0530

    updated menu items

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+        <h2>Delicious Homemade Meals, Delivered to You</h2>
+        <p>Craving something fresh and homely? Discover mouthwatering dishes made by nearby home chefs, ready to be delivered hot to your doorstep.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,14 +2,14 @@ import basket_icon from './basket_icon.png'
 import logo from './zaika.jpg'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
+import menu_1 from './menu_1.jpg'
+import menu_2 from './menu_2.jpg'
+import menu_3 from './menu_3.jpg'
+import menu_4 from './menu_4.jpg'
+import menu_5 from './menu_5.jpg'
+import menu_6 from './menu_6.jpg'
 import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
+import menu_8 from './menu_8.jpg'
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
@@ -84,37 +84,39 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
-        menu_image: menu_1
+      menu_name: "Ghar Ka Khana",
+      menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
-        menu_image: menu_2
+      menu_name: "Roti Sabzi Combo",
+      menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
-        menu_image: menu_3
+      menu_name: "Rice Bowl Specials",
+      menu_image: menu_3
     },
     {
-        menu_name: "Sandwich",
-        menu_image: menu_4
+      menu_name: "South Indian Love",
+      menu_image: menu_4
     },
     {
-        menu_name: "Cake",
-        menu_image: menu_5
+      menu_name: "North Indian Delight",
+      menu_image: menu_5
     },
     {
-        menu_name: "Pure Veg",
-        menu_image: menu_6
+      menu_name: "Biryani Ghar Se",
+      menu_image: menu_6
     },
     {
-        menu_name: "Pasta",
-        menu_image: menu_7
+      menu_name: "Comfort Meals",
+      menu_image: menu_7
     },
     {
-        menu_name: "Noodles",
-        menu_image: menu_8
-    }]
+      menu_name: "Evening Nasta",
+      menu_image: menu_8
+    }
+  ];
+  
 
 export const food_list = [
     {
